10f825a8102d25a8379dac3a786f6d4f1244bdb5
qaPaschaE <chetachi.enyimiri@gmail.com>
2021-07-03 14:52:33 +0100

    implement test search for posts written by user with username=Delphine

--- test/p_app.js
+++ test/p_app.js
@@ -2,16 +2,26 @@ const req = require('supertest');
 const { expect } = require('chai');
 const app = req('http://localhost:3000/');
 
-let arr_response;
+let arr_response, user_posts;
 
 
 describe('GET /users?username=Delphine', () => {
-    it('search for user with username “Delphine"', () => {
+    it('search for user with username “Delphine', () => {
         return app.get('users?username=Delphine').then((res) => {
             arr_response = res.body;
-            expect(res.body).to.be.an('array')
+            expect(arr_response).to.be.an('array')
             expect(res.ok).to.be.true;
-            expect(res.body[0].username).to.be.eq('Delphine');
+            expect(arr_response[0].username).to.be.eq('Delphine');
+        })
+    });
+});
+describe('GET /posts?userId=9', () => {
+    it('search for posts written by user with username “Delphine"', () => {
+        const userId = arr_response[0].id
+        return app.get(`posts?userId=${userId}`).then((res) => {
+            user_posts = res.body;
+            expect(user_posts).to.be.an('array')
+            expect(res.ok).to.be.true;
         })
     });
 });
